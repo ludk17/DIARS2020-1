5c2136461dec893e2689acd2de37f815f2eb54f8
Luigi Mendoza <luigi.mendoza@bit2bitamericas.com>
2020-05-20 19:33:46 -0500

    add js

---
+++ MVCProject/wwwroot/js/main.js
@@ -0,0 +1,34 @@
+﻿
+
+//var $ = jQuery;
+//var h1 = $("h1#titulo");
+//h1.css('color', 'red');
+
+var html = $('#contenido').html();
+console.log(html);
+
+$('#contenido').html("<h1>Cambio de contenido</h1>")
+
+
+for (var i = 0; i < 10; i++) {
+
+}
+
+if (true) {
+
+} else {
+
+}
+
+
+$('button').on('click', function () {    
+
+    $("h1").addClass("red");
+
+    alert($("h1").hasClass("green"));
+
+    $("h1").removeClass("red");   
+
+});
+
+
